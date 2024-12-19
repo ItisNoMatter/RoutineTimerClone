c861e90236353602e7b5e8af63c3177a3823909b
iNoma <702sunnylife@gmail.com>
2024-12-19 15:34:46 +0900

    add Routine data class and its test

---
+++ app/src/main/java/com/example/routinetimerclone/domain/model/Routine.kt
@@ -0,0 +1,21 @@
+package com.example.routinetimerclone.domain.model
+
+data class Routine (
+    val name: String,
+    var tasks: List<Task>,
+)
+{
+    fun getTotalDuration():Duration {
+        var totalseconds = 0
+        for (task in tasks) {
+            totalseconds += task.duration.getTotalSeconds()
+        }
+        return Duration(totalseconds / 60,totalseconds % 60)
+    }
+    fun taskMoved(from: Int, to: Int):Routine {
+        val newTasks: MutableList<Task> = tasks.toMutableList()
+        val task = newTasks.removeAt(from)
+        newTasks.add(to, task)
+        return Routine(name, newTasks)
+    }
+}
---
+++ app/src/test/java/com/example/routinetimerclone/domain/RoutineTest.kt
@@ -0,0 +1,40 @@
+package com.example.routinetimerclone.domain
+
+
+import com.example.routinetimerclone.domain.model.Routine
+import com.example.routinetimerclone.domain.model.Task
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+class RoutineTest {
+    @Test
+    fun totalDurationCalculationTest_empty() {
+        val routine = Routine("Test Routine", emptyList())
+        val (minutes, seconds) = routine.getTotalDuration()
+        assertEquals(0, minutes)
+        assertEquals(0, seconds)
+    }
+    @Test
+    fun taskMovedTest(){
+        val task1 = Task("Task 1", 1, 30)
+        val task2 = Task("Task 2", 2, 0)
+        val task3 = Task("Task 3", 3, 30)
+        val routine = Routine("Test Routine", listOf(task1, task2,task3))
+        val movedRoutine = routine.taskMoved(0, 1)
+        assertEquals(movedRoutine.tasks[0], task2)
+        assertEquals(movedRoutine.tasks[1], task1)
+        assertEquals(movedRoutine.tasks[2], task3)
+    }
+
+    @Test
+    fun totalDurationCalculationTest() {
+        val task1 = Task("Task 1", 1, 30)
+        val task2 = Task("Task 2", 2, 0)
+        val task3 = Task("Task 3", 3, 30)
+        val routine = Routine("Test Routine", listOf(task1, task2,task3))
+        val (minutes, seconds) = routine.getTotalDuration()
+        assertEquals(6, minutes)
+        assertEquals(30, seconds)
+    }
+}
