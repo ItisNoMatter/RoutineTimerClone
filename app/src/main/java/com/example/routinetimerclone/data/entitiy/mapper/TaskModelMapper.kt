c771fd1b277975358013caa7633d4630d9861fc2
iNoma <702sunnylife@gmail.com>
2024-12-29 15:48:07 +0900

    add model mapper

---
+++ app/src/main/java/com/example/routinetimerclone/data/entitiy/mapper/TaskModelMapper.kt
@@ -0,0 +1,20 @@
+package com.example.routinetimerclone.data.entitiy.mapper
+
+import com.example.routinetimerclone.data.entitiy.TaskEntity
+import com.example.routinetimerclone.domain.model.Duration
+import com.example.routinetimerclone.domain.model.Task
+
+class TaskModelMapper {
+    companion object {
+        fun toDomain(taskEntity: TaskEntity): Task {
+            return Task(taskEntity.id, taskEntity.name, Duration.fromSeconds(taskEntity.seconds))
+        }
+
+        fun toEntity(
+            task: Task,
+            parentRoutineId: Int,
+        ): TaskEntity {
+            return TaskEntity(task.id, task.name, task.duration.getTotalSeconds(), parentRoutineId)
+        }
+    }
+}
