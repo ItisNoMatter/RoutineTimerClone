f758ee11355c21318cb2d0e29b9b7a2d6a3f8b0b
iNoma <702sunnylife@gmail.com>
2025-06-09 21:07:32 +0900

    Refactor: Use type-safe navigation and add application name
    This commit introduces type-safe navigation using sealed interfaces for routes.
    It also sets the application name in the AndroidManifest.xml.
    
    Specifically:
    - Defined a `Route` sealed interface with `RoutineList` and `RoutineEdit` as data objects/classes.
    - Updated `EEffortNavGraph` to use these type-safe routes for navigation.
    - Modified `RoutineEdit` to expect a non-null `routineId`.
    - Added `android:name=".EEffortApp"` to the `<application>` tag in `AndroidManifest.xml`.

--- app/src/main/java/com/example/routinetimerclone/MainActivity.kt
+++ app/src/main/java/com/example/routinetimerclone/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import com.example.routinetimerclone.ui.routineEdit.RoutineEditScreen
 import com.example.routinetimerclone.ui.routineList.RoutineListScreen
 import com.example.routinetimerclone.ui.theme.RoutineTimerCloneTheme
@@ -29,35 +30,34 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Serializable
-object RoutineList
+sealed interface Route{
+    @Serializable
+    data object RoutineList:Route
 
-@Serializable
-data class RoutineEdit(
-    val routineId: Long? = null,
-)
+    @Serializable
+    data class RoutineEdit(
+        val routineId: Long,
+    ):Route
+}
 
 @Composable
-fun EEffortNavGraph(startDestination: String = "routineList") {
+fun EEffortNavGraph() {
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = startDestination,
+        startDestination = Route.RoutineList,
     ) {
-        composable("list") {
+        composable<Route.RoutineList> {
             RoutineListScreen(
                 navController = navController,
             )
         }
-        composable(
-            route = "edit/{routineId}",
-            arguments = listOf(navArgument("routineId") { type = NavType.LongType }),
-        ) {
+        composable<Route.RoutineEdit> {
                 backStackEntry ->
             RoutineEditScreen(
                 navHostController = navController,
-                routineId = backStackEntry.arguments?.getLong("routineId")!!,
+                routineId = backStackEntry.toRoute<Route.RoutineEdit>().routineId,
             )
         }
     }
