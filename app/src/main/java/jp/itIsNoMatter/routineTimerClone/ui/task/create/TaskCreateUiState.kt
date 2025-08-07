fcde66334d922e32334c402ffa793ce2f223c62f
iNoma <702sunnylife@gmail.com>
2025-08-07 11:07:12 +0900

    uiStateをsealed classからデータクラスに変更

--- app/src/main/java/jp/itIsNoMatter/routineTimerClone/ui/task/create/TaskCreateUiState.kt
+++ app/src/main/java/jp/itIsNoMatter/routineTimerClone/ui/task/create/TaskCreateUiState.kt
@@ -1,11 +1,17 @@
 package jp.itIsNoMatter.routineTimerClone.ui.task.create
 
+import jp.itIsNoMatter.routineTimerClone.core.LoadedValue
 import jp.itIsNoMatter.routineTimerClone.domain.model.Task
 
-interface TaskCreateUiState {
-    data object Loading : TaskCreateUiState
-
-    data class Done(val task: Task) : TaskCreateUiState
-
-    data class Error(val e: Exception) : TaskCreateUiState
+data class TaskCreateUiState(
+    val task: LoadedValue<Task>,
+    val showDurationInput: Boolean,
+) {
+    companion object {
+        val InitialState =
+            TaskCreateUiState(
+                task = LoadedValue.Loading,
+                showDurationInput = false,
+            )
+    }
 }
