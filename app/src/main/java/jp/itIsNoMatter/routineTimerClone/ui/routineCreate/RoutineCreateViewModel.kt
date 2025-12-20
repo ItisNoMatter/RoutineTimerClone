d0fa9ec6eed9ae77f8804582b0f19509e9ad0e77
iNoma <702sunnylife@gmail.com>
2025-12-20 21:33:17 +0900

    画面遷移の実装

--- app/src/main/java/jp/itIsNoMatter/routineTimerClone/ui/routineCreate/RoutineCreateViewModel.kt
+++ app/src/main/java/jp/itIsNoMatter/routineTimerClone/ui/routineCreate/RoutineCreateViewModel.kt
@@ -8,7 +8,6 @@ import jp.itIsNoMatter.routineTimerClone.data.repository.RoutineRepository
 import jp.itIsNoMatter.routineTimerClone.domain.model.Routine
 import jp.itIsNoMatter.routineTimerClone.ui.navigation.NavEvent
 import jp.itIsNoMatter.routineTimerClone.ui.navigation.Route
-import jp.itIsNoMatter.routineTimerClone.ui.routineEdit.RoutineEditUiState
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -79,20 +78,27 @@ class RoutineCreateViewModel
         }
 
         fun onClickAddTaskButton() {
+            val state = uiState.value
+            if (state !is RoutineCreateUiState.Done) return
             viewModelScope.launch {
+                if (uiState.value is RoutineCreateUiState.Done) {
+                    val routineId = state.routine.id
+                    _navigateTo.emit(NavEvent.NavigateTo(route = Route.TaskCreate(routineId)))
+                }
             }
         }
 
         fun onClickTaskCard(taskId: Long) {
+            val state = uiState.value
+            if (state !is RoutineCreateUiState.Done) return
+            val parentRoutineId = state.routine.id
             viewModelScope.launch {
-                val parentRoutineId = (uiState.value as RoutineEditUiState.Done).routine.id
                 _navigateTo.emit(NavEvent.NavigateTo(route = Route.TaskEdit(parentRoutineId, taskId)))
             }
         }
 
         fun onClickBackButton() {
             val state = uiState.value
-
             viewModelScope.launch {
                 if (state is RoutineCreateUiState.Done && state.routine.isInitial()) {
                     routineRepository.deleteRoutineById(state.routine.id)
